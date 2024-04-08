41976e20e69c05f28791e28ca28f6f91a133361a
Jean-Louis Huynen <huynenjl@gmail.com>
2024-04-08 17:02:47 +0200

    chg: [src] most important functions

--- native/ex_poppy/src/lib.rs
+++ native/ex_poppy/src/lib.rs
@@ -1,18 +1,11 @@
-use std::{
-    fs,
-    io::Error as IoError,
-    path::PathBuf,
-    sync::Mutex,
-};
-
+use poppy::Params;
 use rustler::Error as RustlerError;
 use rustler::ResourceArc;
 use rustler::{Env, Term};
+use std::{fs, io::Error as IoError, path::PathBuf, sync::Mutex};
 use thiserror::Error;
 
-pub struct BloomFilter (
-   Mutex<poppy::BloomFilter>
-);
+pub struct BloomFilter(Mutex<poppy::BloomFilter>);
 
 #[derive(Error, Debug)]
 pub enum ExPoppyError {
@@ -35,25 +28,51 @@ fn load(env: Env, _: Term) -> bool {
 
 #[rustler::nif]
 pub fn new(c: usize, fp: f64) -> ResourceArc<BloomFilter> {
-    ResourceArc::new(BloomFilter (
-        Mutex::new(poppy::BloomFilter::with_capacity(c, fp)),
-    ))
+    ResourceArc::new(BloomFilter(Mutex::new(poppy::BloomFilter::with_capacity(
+        c, fp,
+    ))))
 }
 
 #[rustler::nif]
-pub fn with_version(v: u8, c: usize, fp: f64) -> Result<ResourceArc<BloomFilter>, RustlerError>  {
-    Ok(ResourceArc::new(BloomFilter (
-        Mutex::new(poppy::BloomFilter::with_version_capacity(v, c, fp).map_err(ExPoppyError::from)?),
-    )))
+pub fn with_version(v: u8, c: usize, fp: f64) -> Result<ResourceArc<BloomFilter>, RustlerError> {
+    Ok(ResourceArc::new(BloomFilter(Mutex::new(
+        poppy::BloomFilter::with_version_capacity(v, c, fp).map_err(ExPoppyError::from)?,
+    ))))
 }
 
-/// TODO with params
+#[rustler::nif]
+pub fn with_params(
+    v: u8,
+    c: usize,
+    fp: f64,
+    opt: u8,
+) -> Result<ResourceArc<BloomFilter>, RustlerError> {
+    let p = Params::new(c, fp).version(v).opt(
+        opt.try_into()
+            .map_err(poppy::Error::from)
+            .map_err(ExPoppyError::from)?,
+    );
+
+    Ok(ResourceArc::new(BloomFilter(Mutex::new(
+        p.try_into().map_err(ExPoppyError::from)?,
+    ))))
+}
 
 #[rustler::nif]
 /// Insert a str into the filter
 pub fn insert_str(bf: ResourceArc<BloomFilter>, s: &str) -> Result<bool, RustlerError> {
-    Ok(bf.0.lock().unwrap().insert_bytes(s).map_err(ExPoppyError::from)?)
+    Ok(bf
+        .0
+        .lock()
+        .unwrap()
+        .insert_bytes(s)
+        .map_err(ExPoppyError::from)?)
 }
+
+// #[rustler::nif]
+// pub fn insert_bytes(&mut self, data: &[u8]) -> Result<bool, RustlerError>  {
+//     Ok(bf.0.lock().unwrap().insert_bytes(data).map_err(ExPoppyError::from)?)
+// }
 
 /// Check if argument is contained in the filter
 #[rustler::nif]
@@ -61,13 +80,26 @@ pub fn contains_str(bf: ResourceArc<BloomFilter>, s: &str) -> bool {
     bf.0.lock().unwrap().contains_bytes(s)
 }
 
+// #[rustler::nif]
+// pub fn contains_bytes(bf: ResourceArc<BloomFilter>, data: &[u8]) -> bool {
+//     bf.0.lock().unwrap().contains_bytes(data)
+// }
+
 #[rustler::nif]
 pub fn load_filter(path: String) -> Result<ResourceArc<BloomFilter>, RustlerError> {
     let path = PathBuf::from(path);
     let bf = poppy::BloomFilter::from_reader(fs::File::open(&path).map_err(ExPoppyError::from)?)
         .map_err(ExPoppyError::from)?;
 
-    Ok(ResourceArc::new(BloomFilter (Mutex::new(bf))))
+    Ok(ResourceArc::new(BloomFilter(Mutex::new(bf))))
+}
+
+#[rustler::nif]
+/// Save filter into a file
+pub fn save(bf: ResourceArc<BloomFilter>, path: String) -> Result<(), RustlerError> {
+    let path = PathBuf::from(path);
+    let mut f = fs::File::create(path).map_err(ExPoppyError::from)?;
+    Ok(bf.0.lock().unwrap().write(&mut f).map_err(ExPoppyError::from)?)
 }
 
 #[rustler::nif]
@@ -100,14 +132,18 @@ rustler::init!(
     [
         new,
         with_version,
+        with_params,
         insert_str,
+        // insert_bytes,
         contains_str,
+        // contains_bytes,
         version,
         capacity,
         fpp,
         count_estimate,
         data,
-        load_filter
+        load_filter,
+        save
     ],
     load = load
 );
